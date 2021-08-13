61d687da7e57cb2b486c280695b612d057d11a94
Abdul Imad <abdimad786@gmail.com>
2021-08-13 22:57:44 +0530

    realtime voting update

--- src/components/CreatePoll.js
+++ src/components/CreatePoll.js
@@ -2,73 +2,79 @@ import React, { useState } from "react";
 import { database } from "../firebase/firebaseConfig";
 
 export default function CreatePoll(props) {
-  const [question, setQuestion] = useState("");
-  const [options, setOptions] = useState(["", ""]); 
+	const [question, setQuestion] = useState("");
+	const [options, setOptions] = useState(["", ""]);
 
-  const handleCreate = async () => {
-    let poll = {
-      question,
-      options,
-      totalVotes: 0,
-      votesToEachOption: [0, 0],
-    };
-    try {
-      let resp = database.polls.doc();
-      let pollId = resp.id;
-      await resp.set(poll);
-      let iop = JSON.parse(localStorage.getItem("pollIds"));
-      let oldPollIds = iop == null ? [] : iop;
-      oldPollIds.push(pollId);
-      console.log(oldPollIds);
-      localStorage.setItem("pollIds", JSON.stringify(oldPollIds));
-      props.history.push(`/poll/${pollId}`);
-    } catch (err) {
-      console.log("Error :", err);
-    }
-  };
+	const handleCreate = async () => {
+		const optionAndVotesArr = options.map((option) => {
+			return { 
+                optionName : option,
+                votes : 0
+             };
+		});
+		let poll = {
+			question,
+			options,
+			totalVotes: 0,
+			votesToEachOption: optionAndVotesArr,
+		};
+		try {
+			let resp = database.polls.doc();
+			let pollId = resp.id;
+			await resp.set(poll);
+			let iop = JSON.parse(localStorage.getItem("pollIds"));
+			let oldPollIds = iop == null ? [] : iop;
+			oldPollIds.push(pollId);
+			console.log(oldPollIds);
+			localStorage.setItem("pollIds", JSON.stringify(oldPollIds));
+			props.history.push(`/poll/${pollId}`);
+		} catch (err) {
+			console.log("Error :", err);
+		}
+	};
 
-  const handleAddOption = () => {
-    setOptions([...options, ""]);
-  };
+	const handleAddOption = () => {
+		setOptions([...options, ""]);
+	};
 
-  const handleDelete = (e) => {
-    let idx = Number(e.target.parentNode.id);
-    let tempOptions = [...options];
-    let UpdatedOptions = []
-    for(let i = 0; i < tempOptions.length; i++){
-        if(i != idx){
-            UpdatedOptions.push(tempOptions[i])
-        }
-    }
-    setOptions(UpdatedOptions);
-};
+	const handleDelete = (e) => {
+		let idx = Number(e.target.parentNode.id);
+		let tempOptions = [...options];
+		let UpdatedOptions = [];
+		for (let i = 0; i < tempOptions.length; i++) {
+			if (i != idx) {
+				UpdatedOptions.push(tempOptions[i]);
+			}
+		}
+		setOptions(UpdatedOptions);
+	};
 
-  return (
-    <div>
-      <input
-        placeholder="question"
-        onChange={(e) => setQuestion(e.target.value)}
-      ></input>
-      {options.map((option, idx) => {
-        return (
-          <div key={idx} id={idx}>
-            <input
-              placeholder="option"
-              value={option}
-              onChange={(e) => {
-                let tempArr = [...options];
-                tempArr[idx] = e.target.value;
-                setOptions(tempArr);
-              }}
-            ></input>
-            {options.length > 2 ? (
-              <button onClick={handleDelete}>delete</button>
-            ) : null}
-          </div>
-        );
-      })}
-      <button onClick={handleAddOption}>Add Option</button>
-      <button onClick={handleCreate}>Create</button>
-    </div>
-  );
+	return (
+		<div>
+			<input
+				placeholder="question"
+				onChange={(e) => setQuestion(e.target.value)}
+			></input>
+			{options.map((option, idx) => {
+				return (
+					<div key={idx} id={idx}>
+						<input
+							placeholder="option"
+							value={option}
+							onChange={(e) => {
+								let tempArr = [...options];
+								tempArr[idx] = e.target.value;
+								setOptions(tempArr);
+							}}
+						></input>
+						{options.length > 2 ? (
+							<button onClick={handleDelete}>delete</button>
+						) : null}
+					</div>
+				);
+			})}
+			<button onClick={handleAddOption}>Add Option</button>
+			<button onClick={handleCreate}>Create</button>
+		</div>
+	);
 }
--- src/components/PollResult.js
+++ src/components/PollResult.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { database } from "../firebase/firebaseConfig";
 
 export default function PollResult(props) {
+	const [question, setQuestion] = useState("");
+	const [options, setOptions] = useState([]);
+	const [totalVotes, setTotalVotes] = useState(0);
+	const [eachOptionVotes, setEachOptionVotes] = useState([]);
 	const {
 		match: {
 			params: { id },
 		},
 	} = props;
 	console.log(id);
-	return <div>Poll Result</div>;
+
+	useEffect(() => {
+		database.polls.doc(id).onSnapshot((doc) => {
+			let data = doc.data();
+			setTotalVotes(data.totalVotes);
+			setQuestion(data.question);
+			setOptions(data.options);
+            let sortedArr = data.votesToEachOption.sort((a,b)=>{
+                return b.votes - a.votes 
+            })
+			setEachOptionVotes(sortedArr);
+			console.log(totalVotes);
+		});
+	}, []);
+	return (
+		<>
+			<div>
+				<h1>{question}</h1>
+				{eachOptionVotes.map((option, id) => {
+					return (
+						<div key={id}>
+							<h2>{option.optionName}</h2>
+							<span>{option.votes}</span>
+						</div>
+					);
+				})}
+			</div>
+			<div>Total Votes : {totalVotes}</div>
+		</>
+	);
 }
