fe4d2521d73d38ba40972814e6274a0bcda64ff5
Abdul Imad <abdimad786@gmail.com>
2021-08-18 16:56:08 +0530

    .

--- src/components/MyPolls.js
+++ src/components/MyPolls.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { database } from "../firebase/firebaseConfig";
+import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
